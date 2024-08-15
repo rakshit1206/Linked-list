355604ea8bd8cc717b43ac87297e19443333acb4
Rakshit Sain <rakshitsain895@gmail.com>
2024-08-15 18:51:32 -0400

    prepend method added

---
+++ src/LinkedList.js
@@ -0,0 +1,22 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  prepend(value) {
+    const newNode = new Node(value);
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+  }
+}
